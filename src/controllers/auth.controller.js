8ae4e3c5e2a80f19b2f594f9b0944922c092063d
TanishJangra <96981050+TanishJangra@users.noreply.github.com>
2026-04-01 19:40:42 +0530

    first commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+const authController = {};
+
+const userModel = require('../model/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookies = require('cookie-parser');
+
+authController.registerUserController = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        if(!username || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const existingUser = await userModel.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username or email already exists' });
+        }
+        const hash = await bcrypt.hash(password, 10);
+        const newUser = new userModel({ username, email, password: hash });
+        const token = jwt.sign({ id: newUser._id, username: newUser.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.cookie("token", token);
+        await newUser.save();
+        res.status(201).json({ message: 'User registered successfully', user: {
+            id: newUser._id,
+            username: newUser.username,
+            email: newUser.email
+        }});
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+authController.loginUserController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.cookie("token", token);
+        res.status(200).json({ message: 'User logged in successfully', user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }});
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+module.exports = authController;
