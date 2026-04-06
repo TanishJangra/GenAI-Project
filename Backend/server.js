f4248c571702054639a20377d03dbbbac49781b0
TanishJangra <96981050+TanishJangra@users.noreply.github.com>
2026-04-06 21:59:51 +0530

    interview Report schema setup to generate json format data based on prompt provided

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
-// const {invoke} = require('./src/services/ai.service.js');
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
+
 connectDB();
-// invoke();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -19,27 +19,48 @@ const invoke = async () => {
     console.log("response from ai: ",response.text);
 }
 
+const interviewReportSchema = z.object({
+    matchScore: z.number().min(0).max(100).describe("The match score between the candidate and the job describe, which indicates how well the candidate's resume and self-describe match the requirements of the job describe"),
+    technicalQuestions: z.array(z.object({
+        question: z.string().describe("The technical question can be asked during the interview"),
+        intention: z.string().describe("The intention of interviewer behind asking the technical question"),
+        answer: z.string().describe("How to answer this question, what points to cover, how to structure the answer, etc."),
+    })).describe("Technical questions that can be asked during the interview along with the intention behind asking those questions and how to answer them"),
+    behavioralQuestions: z.array(z.object({
+        question: z.string().describe("The behavioral question can be asked during the interview"),
+        intention: z.string().describe("The intention of interviewer behind asking the behavioral question"),
+        answer: z.string().describe("How to answer this question, what points to cover, how to structure the answer, etc."),
+    })).describe("Behavioral questions that can be asked during the interview along with the intention behind asking those questions and how to answer them"),
+    skillGaps: z.array(z.object({
+        skill: z.string().describe("The skill in which the candidate is lacking"),
+        severity: z.enum(['Low', 'Medium', 'High']).describe("The severity of the skill gap"),
+    })).describe("The skills in which the candidate is lacking along with the severity of those skill gaps"),
+    preparationPlan: z.array(z.object({
+        day: z.number().describe("The day number in the preparation plan"),
+        focus: z.string().describe("The main focus of the day in the preparation plan e.g, data structures, system design, mock interviews, etc."),
+        tasks: z.array(z.string()).describe("The tasks to be completed on that day in the preparation plan"),
+    })).describe("The preparation plan for the candidate to prepare for the interview, which includes the day number, main focus of the day, and the tasks to be completed on that day")
+});
+
 async function generateInterviewReport({resume, selfDescription, jobDescription}) {
-    const schema = z.object({
-        strengths: z.array(z.string()),
-        weaknesses: z.array(z.string()),
-        suggestions: z.array(z.string()),
-    });
-    const jsonSchema = zodToJsonSchema(schema);
+    
+    const prompt = `Generate an interview report for a candidate with the following details:
+                        Resume: ${resume}
+                        Self Description: ${selfDescription}
+                        Job Description: ${jobDescription}`
+
     const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        contents: [
-            {
-                role: "system",
-                parts: [{ text: "You are an expert career counselor providing feedback on job applications." }]
-            },
-            {
-                role: "user",
-                parts: [{ text: `Please analyze the following resume, self-description, and job description to provide feedback on the candidate's strengths, weaknesses, and suggestions for improvement. Format your response according to the following JSON schema: ${JSON.stringify(jsonSchema)}. Resume: ${resume} Self-description: ${selfDescription} Job Description: ${jobDescription}` }]
-            }
-        ]
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseJsonSchema: zodToJsonSchema(interviewReportSchema),
+        }
     });
-    return response.choices[0].message.parts[0].text;
+
+    const interviewReport = JSON.parse(response.text);
+    console.log("Generated Interview Report: ", interviewReport);
+    return interviewReport;
 }
 
 module.exports = { invoke, generateInterviewReport };
